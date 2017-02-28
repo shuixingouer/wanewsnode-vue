f5c541db0119929a89c6a67386799a32b9e40bd5
shuixing <shuixing@ixiaopu.com>
2017-02-28 12:06:09 +0800

    vue前端搭建，从express的服务器中链接api获取数据

---
+++ src/components/ChannelMenu.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="channel-menu">
+    <div class="weui-panel">
+    <div class="weui-navbar" style="position:static">
+    <div class="weui-navbar__item" v-for="(item,$index) in channel" :class="{'weui-bar__item_on': $index === isActive}" @click="cutOff($index)">
+      {{item.channel_name}}
+    </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { bus } from '../bus.js'
+export default {
+  name: 'channelmenu',
+  data () {
+    return {
+      isActive: 0,
+      channel: []
+    }
+  },
+  created () {
+    this.getLists()
+  },
+  methods: {
+    cutOff: function (index) {
+      this.isActive = index
+      bus.$emit('tip', index)
+    },
+    getLists: function () {
+      this.$http.post('/api/channel/list', {}).then((response) => {
+        this.channel = response.body
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page, body{
+  background:#fff;
+}
+.weui-panel{
+  overflow:auto;
+}
+.weui-navbar{
+  background:#ebebeb;
+}
+.weui-navbar__item{
+  padding:13px 10px 13px;
+  font-size:16px;
+  white-space:nowrap;
+  background:#ebebeb;
+  cursor:pointer;
+}
+.weui-navbar__item:after{
+  border-right:none;
+}
+.weui-navbar__item.weui-bar__item_on{
+  color:rgb(244, 67, 54);
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="list">
+    <div>{{tip}}</div>
+    <a href="" class="weui-media-box weui-media-box_appmsg"  v-for="item in listData">
+      <div class="weui-media-box__hd">
+      </div>
+      <div class="weui-media-box__bd">
+        <h4 class="weui-media-box__title">{{item.title}}</h4>
+        <p class="weui-media-box__desc">{{item.genres}}</p>
+        <p class="weui-media-box__time">{{item.subtype}}</p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+  import { bus } from '../bus.js'
+  export default {
+    name: 'list',
+    data () {
+      return {
+        list: [{
+          imgs: './assets/images/logo.png',
+          title: 'Jack',
+          desc: 'Jack',
+          time: 'Jack'
+        }, {
+          imgs: './assets/images/logo.png',
+          title: 'Jack',
+          desc: 'Jack',
+          time: 'Jack'
+        }],
+        listData: [],
+        tip: '0'
+      }
+    },
+    created () {
+      bus.$on('tip', (text) => {
+        this.tip = text
+        this.getLists(text)
+      })
+    },
+    methods: {
+      getLists: function (id) {
+        this.$http.post('/api/channel/' + id, {}).then((response) => {
+          console.log(response)
+        })
+      }
+    }
+  }
+</script>
